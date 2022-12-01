8b0540c1092d329057dd2da1bd7140c88138bce0
githubdene <119507920+githubdene@users.noreply.github.com>
2022-12-01 17:45:55 +0300

    Update logic.js

--- logic.js
+++ logic.js
@@ -1,4 +1,4 @@
-var searchResultFormat = '<tr><td><a href="$link" target="_blank">$target</a></td><td align="left">$title</td><td>$tag</tr>';
+var searchResultFormat = '<tr><td><a href="$link" target="_blank">$target</a></td><td align="left">$title</td></tr>';
 var totalLimit = 750;
 
 var controls = {
